e2d9234f8d4cc0f69c8d559db4a8c81adb8e2731
Andreas Vasiliou <andreas.vasiliou14@outlook.com>
2022-06-05 07:27:40 +0300

    fix references

--- tests/Web/Application.UnitTests/Common/Behaviours/RequestLoggerTests.cs
+++ tests/Web/Application.UnitTests/Common/Behaviours/RequestLoggerTests.cs
@@ -1,7 +1,4 @@
-﻿using CleanArchitecture.Application.Common.Behaviours;
-using CleanArchitecture.Application.Common.Interfaces;
-using CleanArchitecture.Application.TodoItems.Commands.CreateTodoItem;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Logging;
 using Moq;
 using NUnit.Framework;
 
